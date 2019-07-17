1a5e3d014ec0a41db23c4abe21d88b470c5fd3aa
gaganVirk <deep.virk@outlook.co.nz>
2019-07-17 14:44:29 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topolgical_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("rsg.txt");
+            Console.SetIn(reader);
+            int size = Convert.ToInt32(Console.ReadLine());
+            List<Vertex> vertices = new List<Vertex>();
+ 
+            for(int i = 0; i < size; i++)
+            {
+                vertices.Add(new Vertex(i));
+            }
+
+            string line = Console.ReadLine();
+            while(line != null)
+            {
+                string[] array = line.Split(' ');
+                int token1 = int.Parse(array[0]);
+                int token2 = int.Parse(array[1]);
+                vertices[token1].AddNeighbour(vertices[token2]);
+                line = Console.ReadLine();
+            }
+
+            foreach(Vertex v in vertices)
+            {
+                Console.WriteLine(v.Description);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vertex.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Topolgical_Algorithm
+{
+    class Vertex : IComparable<Vertex>
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Incoming { get; set; }
+        List<Vertex> adj = new List<Vertex>();
+
+        public Vertex(int id)
+        {
+            this.Id = id;
+        }
+
+        public void AddNeighbour(Vertex v)
+        {
+            adj.Add(v);
+        }
+
+        public override string ToString()
+        {
+            adj.Sort();
+            string s = "";
+            foreach(Vertex v in adj)
+            {
+                s += string.Format("{0}", v.Description);
+            }
+            return s;
+        }
+
+        public int CompareTo(Vertex other)
+        {
+           return Id.CompareTo(other.Id);
+        }
+    }
+}
